d73c49226b8f396cfcbf262d7c8e6f2ccb905166
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-04-07 18:08:12 +0530

    Intial commit

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+let nCount = selector => {
+  $(selector).each(function () {
+    $(this)
+      .animate({
+        Counter: $(this).text()
+      }, {
+        // A string or number determining how long the animation will run.
+        duration: 5000,
+        // A string indicating which easing function to use for the transition.
+        easing: "swing",
+        /**
+         * A function to be called for each animated property of each animated element. 
+         * This function provides an opportunity to
+         *  modify the Tween object to change the value of the property before it is set.
+         */
+        step: function (value) {
+          $(this).text(Math.ceil(value));
+        }
+      });
+  });
+};
+
+let a = 0;
+  // The .offset() method allows us to retrieve the current position of an element  relative to the document
+  if (a == 0) {
+    a++;
+    nCount(".animCounter > h2");
+  }
